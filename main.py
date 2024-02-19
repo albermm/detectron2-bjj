eb7d9f037255b3a8b2c82e1c2f90fd957089ac2b
Alberto Martín <alberto.martin@cantab.net>
2024-02-19 20:35:02 +1300

    new main working well for the model selection

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import cv2
 from utils.helper import Detector
 from argparse import ArgumentParser
-import sys
-import matplotlib.pyplot as plt
-import numpy as np
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -12,24 +9,22 @@ parser.add_argument(
 parser.add_argument(
     "--out", type=str, help="Set the output path to the video", required=True
 )
+parser.add_argument(
+    "--model_type", type=str, help="Set the model type (OD, IS, KP)", required=True
+)
 args = parser.parse_args()
 
-
-
-predictor = Detector(model_type="KP")
-
-
+# Create the predictor with the specified model_type
+predictor = Detector(model_type=args.model_type)
 
 if __name__ == "__main__":
     # Example usage for processing a single image
-    predictor = Detector()
     image_path = args.input
     out_frame, out_frame_seg = predictor.onImage(image_path)
+    
     # Now you can do something with the results, such as displaying or saving them
-
 
     # Write the frame to the output image
     output_image_path = args.out
     cv2.imwrite(output_image_path, out_frame_seg)
-
 
