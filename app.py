9839a0fc356a978d5d99f853be27f9b168461b3b
Alberto Martín <alberto.martin@cantab.net>
2024-08-08 13:47:59 +1200

    new app flask ports

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def home():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-    
+
 # AWS S3 Configuration
 S3_BUCKET = os.getenv('S3_BUCKET')
 S3_REGION = os.getenv('S3_REGION')
