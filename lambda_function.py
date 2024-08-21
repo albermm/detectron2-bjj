79e7040043092a36b67766b5c4c294a86e3580f3
Alberto Martín <alberto.martin@cantab.net>
2024-08-21 14:43:00 +1200

    new API to handle lambda requests

--- lambda_function.py
+++ lambda_function.py
@@ -1,47 +1,29 @@
 import boto3
-import cv2
-import numpy as np
-from utils.helper import Predictor
-from utils.find_position import find_position
+import requests
+import json
 
 s3_client = boto3.client('s3')
-BUCKET_NAME = 'bjj-pics'
 
 def lambda_handler(event, context):
-    # Get the uploaded file information
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
-    # Exit early if the object is in the outputs/ prefix
+    # Skip processing for already processed images
     if key.startswith('outputs/'):
-        print(f"Skipping processing for output file: {key}")
         return {
             'statusCode': 200,
-            'body': 'Skipped processing output file'
+            'body': json.dumps('Skipping processed image')
         }
 
-    # Download the image from S3
-    local_file_name = '/tmp/image.jpg'
-    s3_client.download_file(bucket, key, local_file_name)
+    # Call the EC2 API to process the image
+    ec2_url = "http://http://3.90.218.53:5000/process_image"
+    response = requests.post(ec2_url, json={'file_name': key})
+    response_data = response.json()
 
-    # Process the image
-    predictor = Predictor()
-    output_path = '/tmp/output'
-    keypoint_frame, densepose_frame, keypoints, densepose = predictor.onImage(local_file_name, output_path)
-
-    # Find position
-    predicted_position = find_position(keypoints)
-
-    # Upload results back to S3
-    original_filename = key.split('/')[-1]
-    keypoints_key = f"outputs/keypoints_{original_filename}"
-    densepose_key = f"outputs/densepose_{original_filename}"
-    s3_client.upload_file(f'{output_path}_keypoints.jpg', BUCKET_NAME, keypoints_key)
-    s3_client.upload_file(f'{output_path}_densepose.jpg', BUCKET_NAME, densepose_key)
-
-    # Here you would typically store results in DynamoDB or another database
+    # Handle the response as needed
+    print(f"EC2 response: {response_data}")
 
     return {
         'statusCode': 200,
-        'body': 'Image processed successfully'
+        'body': json.dumps('Processing complete!')
     }
