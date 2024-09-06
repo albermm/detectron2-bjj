63cf5b4afe9d5ce03bbedd87f48a8e1c082c2204
Alberto Martín <alberto.martin@cantab.net>
2024-09-06 15:26:28 +1200

    restaurado get_upload_url que habia borrado por error

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ app = Flask(__name__)
 
 s3_client = boto3.client('s3', config=Config(signature_version='s3v4'))
 BUCKET_NAME = 'bjj-pics'
+
+@app.route('/get_upload_url', methods=['GET'])
+def get_upload_url():
+    file_name = f"inputs/{uuid.uuid4()}.jpg"
+    presigned_url = s3_client.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': BUCKET_NAME, 'Key': file_name},
+        ExpiresIn=3600
+    )
+    return jsonify({'upload_url': presigned_url, 'file_name': file_name})
 @app.route('/process_image', methods=['POST'])
 def process_image():
     data = request.json
